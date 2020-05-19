17882d0bb9d6b50bc41c6561583b83827fc05d84
Yinhao Li <frankleesd@gmail.com>
2020-05-19 21:48:51 +0100

    Create docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,29 @@
+ #!/bin/bash
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+#1 Change dir
+cd /etc/systemd/system/
+
+#2 ensure chmod permission properties are set
+sudo chmod -R 777 /etc/systemd/system/
+
+#3
+mkdir docker.service.d
+cd docker.service.d
+
+cat > startup_options.conf << EOF
+NameVirtualHost 127.0.0.1
+
+# Default
+/etc/systemd/system/docker.service.d/override.conf
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:2376
+</VirtualHost>
+EOF
+
+#4 restart daemon
+sudo systemctl daemon-reload
+
+#5 restart docker
+sudo systemctl restart docker
